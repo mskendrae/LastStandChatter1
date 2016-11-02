332c9cd10932c482e68b447a208ed39a6d93b7ef
Kendra Egert <Kendra Egert>
2016-11-01 20:37:03 -0400

    removed email

--- Chatter/Models/AccountViewModels.cs
+++ Chatter/Models/AccountViewModels.cs
@@ -53,11 +53,7 @@ namespace Chatter.Models
         [Display(Name = "User Name")]
         public string Username { get; set; }
 
-        //[Required]
-        //[Display(Name = "Email")]
-        //[EmailAddress]
-        //public string Email { get; set; }
-
+       
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
@@ -73,12 +69,7 @@ namespace Chatter.Models
         [Display(Name = "User Name")]
         public string Username { get; set; }
 
-        //[Required]
-        //[EmailAddress]
-        //[Display(Name = "Email")]
-        //public string Email { get; set; }
-
-        [Required]
+            [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
